326a60a02df2e769ba621c89edc1150fec488687
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-01-29 11:10:07 +0100

    COMMIT GITHUB

---
+++ src/exemploalumno/ExemploAlumno.java
@@ -0,0 +1,18 @@
+
+package exemploalumno;
+import datos_academicos.NotasAlumnos;
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class ExemploAlumno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NotasAlumnos obxAl=new NotasAlumnos("David",7);
+        System.out.println(obxAl.toString());
+    }
+    
+}
